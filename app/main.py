62776af6e7a2036a04c84c99e6f864c3f43f7e5e
Anjaneya <anjaneyak10@gmail.com>
2024-04-07 15:23:27 -0400

    Adding CORS for integration with angular

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import os
+from flask_cors import CORS
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from api import controller
@@ -7,6 +7,7 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'static'
 app.register_blueprint(controller)
 load_dotenv()
+CORS(app)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9083)
